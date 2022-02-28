d3db700c30e39cb7e4c754afc3899b2e91caf365
rjagtani <rjagtani@gmail.com>
2022-02-28 18:55:29 +0100

    first refactor

--- src/main/java/org/example/MyResource.java
+++ src/main/java/org/example/MyResource.java
@@ -1,5 +1,8 @@
 package org.example;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import jakarta.json.JsonNumber;
+import jakarta.json.JsonObject;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 
@@ -16,15 +19,30 @@ public class MyResource {
      * @return String that will be returned as a text/plain response.
      */
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
-//    public String getIt() {
-//        return "Got it!";
-//    }
+    @Produces(MediaType.APPLICATION_JSON)
     public String test(
-            @QueryParam("originLat") @DefaultValue("8.681495") float start_lat,
-            @QueryParam("originLon") @DefaultValue("49.41461") float start_lon,
-            @QueryParam("destinationLat") @DefaultValue("8.686507") float end_lat,
-            @QueryParam("destinationLon") @DefaultValue("49.41943") float end_lon
+            @QueryParam("originLat") @DefaultValue("8.681495") double start_lat,
+            @QueryParam("originLon") @DefaultValue("49.41461") double start_lon,
+            @QueryParam("destinationLat") @DefaultValue("8.686507") double end_lat,
+            @QueryParam("destinationLon") @DefaultValue("49.41943") double end_lon
     ) { return ShortestPath.RouteFinder(start_lat, start_lon, end_lat, end_lon); }
+
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public String post(JsonObject postRequest) throws JsonProcessingException {
+        JsonNumber start_latJson = (JsonNumber) postRequest.get("originLat");
+        JsonNumber start_lonJson = (JsonNumber) postRequest.get("originLon");
+        JsonNumber end_latJson = (JsonNumber) postRequest.get("destinationLat");
+        JsonNumber end_lonJson = (JsonNumber) postRequest.get("destinationLon");
+        double start_lat = (float) start_latJson.doubleValue();
+        double start_lon = start_lonJson.doubleValue();
+        double end_lat = end_latJson.doubleValue();
+        double end_lon = end_lonJson.doubleValue();
+        //String route = RequestDirection.getRoute(originLat, originLong, destinationLat, destinationLong);
+
+        return ShortestPath.RouteFinder(start_lat, start_lon, end_lat, end_lon);
+    }
 }
 // http://localhost:9090/sysdev/orsdirections?originLat=8.681495&originLon=49.41461&destinationLat=8.686507&destinationLon=49.41943
